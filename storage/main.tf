48bdc4294f932194ef1cd9ba3bd87d8435835649
Daniel Niemczok <biuro@tmask.pl>
2023-10-31 09:18:45 +0100

    gcp-object

--- storage/main.tf
+++ storage/main.tf
@@ -2,9 +2,22 @@ resource "google_storage_bucket" "bucket-from-tf1" {
     name = "bucket-from-tf1"
     location = "europe-central2"
     labels = {
-      "gcp" = "cloud-storage"
-      "terraform" = "storage-branch"
+        "gcp" = "cloud-storage"
+        "terraform" = "storage-branch"
     }
     storage_class = "NEARLINE"
-    uniform_bucket_level_access = true
+    uniform_bucket_level_access = false
+}
+
+resource "google_storage_bucket_object" "gcp-object" {
+    name = "gcp-object"
+    bucket = google_storage_bucket.bucket-from-tf1.name
+    source = "./Kris-Pol.pdf"
+}
+
+resource "google_storage_object_access_control" "view-rule" {
+    bucket = google_storage_bucket.bucket-from-tf1.name
+    object = google_storage_bucket_object.gcp-object.name
+    role = "READER"
+    entity = "allUsers"
 }
